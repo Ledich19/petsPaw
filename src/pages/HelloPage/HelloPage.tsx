d57a559dd5a07bf6f76a9a7ccf3f29e05bf96108
Aleksandr <ledich19@gmail.com>
2023-08-24 19:24:14 +0300

    fix: make responsible hello page

--- src/pages/HelloPage/HelloPage.tsx
+++ src/pages/HelloPage/HelloPage.tsx
@@ -2,8 +2,10 @@ import s from './HelloPage.module.scss';
 
 const HelloPage = () => {
   return (
-    <div className={s.wrapper}>
-      <img className={s.img} src="img/hello.png" alt="man to hug cat" />
+    <div className={s.hello}>
+      <div className={s.wrapper}>
+        <img className={s.img} src="img/hello.png" alt="man to hug cat" />
+      </div>
     </div>
   );
 };

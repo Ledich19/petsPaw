a38141798949c4ab6f4014be39da273cde762823
Aleksandr <ledich19@gmail.com>
2023-08-24 16:49:57 +0300

    style: main structure

---
+++ src/components/NavigationItem/NavigationItem.tsx
@@ -0,0 +1,8 @@
+import { NavLink } from 'react-router-dom';
+import s from './NavigationItem.module.scss';
+
+const NavigationItem = () => {
+  return <div className={s.header}></div>;
+};
+
+export default NavigationItem;
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from '../../components/Navigation/Navigation';
+import s from './Layout.module.scss';
 
 const Layout = () => {
   return (
-    <>
+    <div className={s.container}>
       <Navigation />
       <Outlet />
-    </>
+    </div>
   );
 };
 

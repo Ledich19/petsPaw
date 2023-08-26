ead5bb5d6ee15e860dd79b69ea4fc8f06597999b
Aleksandr <ledich19@gmail.com>
2023-08-26 12:11:01 +0300

    feat: add breeds page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ErrorPage from './pages/Page404/Page404';
 import Layout from './pages/Layout/Layout';
 import HelloPage from './pages/HelloPage/HelloPage';
 import Votes from './pages/VotingBody/Votes';
+import Breeds from './pages/Breeds/Breeds';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Route path="*" element={<Layout />}>
         <Route path="/*" element={<HelloPage />} />
         <Route path="voting" element={<Votes />} />
-        <Route path="breeds" element={<>breeds</>} />
+        <Route path="breeds" element={<Breeds />} />
         <Route path="breeds/:id" element={<>breeds/:id</>} />
         <Route path="gallery" element={<>gallery</>} />
         <Route path="404" element={<ErrorPage />} />

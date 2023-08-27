c1265c5c4ac4ddaa18bcbda20245e0f459c73106
Aleksandr <ledich19@gmail.com>
2023-08-27 14:35:50 +0300

    fet: add history.back to back btn

--- src/components/BackBtn/BackBtn.tsx
+++ src/components/BackBtn/BackBtn.tsx
@@ -2,7 +2,7 @@ import s from './BackBtn.module.scss';
 
 const BackBtn = () => {
   return (
-    <button type="button" className={s.btn}>
+    <button onClick={() => window.history.back()} type="button" className={s.btn}>
       <span className="icon-back" />
     </button>
   );

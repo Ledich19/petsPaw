80085bdd5d7b74f170b092685c28ba522a791e44
Aleksandr <ledich19@gmail.com>
2023-08-28 01:14:18 +0300

    fix: fix search panel

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,16 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navigation from '../../components/Navigation/Navigation';
 import s from './Layout.module.scss';
 import SearchPanel from '../../components/SearchPanel/SearchPanel';
 import Modal from '../Modal/Modal';
 
 const Layout = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
   return (
     <div className={s.layout}>
       <Modal />
       <Navigation />
       <div className={s.appBody}>
-        <SearchPanel />
+        {!isHomePage && <SearchPanel />}
         <Outlet />
       </div>
     </div>
